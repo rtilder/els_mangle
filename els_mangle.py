38567b2cf404c455d06e782ad8f7f7f0244fc189
Ryan Tilder <ryan@tilder.org>
2017-06-20 11:07:13 -0700

    Add simple SQLite support and use tab delimiters

--- els_mangle.py
+++ els_mangle.py
@@ -3,20 +3,75 @@
 import argparse
 import csv
 import json
+import sqlite3
 import sys
 
 import woothee
 
 
+class SQLiteWriter(object):
+
+    SQL_TABLE = """
+        CREATE TABLE agents (
+            ua TEXT,
+            os TEXT,
+            osv TEXT,
+            dtype TEXT,
+            bname TEXT,
+            version TEXT
+        );
+    """
+
+    SQL_INSERT = "INSERT INTO agents VALUES (?, ?, ?, ?, ?, ?);"
+
+    def __init__(self, path):
+        self._buffer = []
+        self._buf_len = 1000
+
+        self.conn = sqlite3.connect(path)
+        c = self.conn.cursor()
+        c.execute(self.SQL_TABLE)
+        self.conn.commit()
+
+    def writerow(self, args):
+        self._buffer.append(tuple(args))
+        if len(self._buffer) >= self._buf_len:
+            self._writerow()
+
+    def _writerow(self):
+        c = self.conn.cursor()
+        c.executemany(self.SQL_INSERT, self._buffer)
+        self.conn.commit()
+        self._buffer = []
+
+    def __del__(self):
+        self._writerow()
+        self.conn.close()
+
+
+def new_sqlite_writer(path):
+    return SQLiteWriter(path)
+
+
+def new_csv_writer(handle):
+    return csv.writer(handle, delimiter='\t')
+
+
 def main(args):
     parser = argparse.ArgumentParser()
+    parser.add_argument('--sql', action='store', default=None,
+                        help='the filename of the SQLite DB to create')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
                         default=sys.stdin)
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'),
                         default=sys.stdout)
     args = parser.parse_args(args)
 
-    csvwriter = csv.writer(args.outfile)
+    if args.sql:
+        writer = new_sqlite_writer(args.sql)
+    else:
+        writer = new_csv_writer(args.outfile)
+
     while True:
         line = args.infile.readline().strip()
         if not line:
@@ -27,13 +82,14 @@ def main(args):
         device_type = log_entry['client']['deviceType']
         ua_string = log_entry['clientRequest']['userAgent']
         woot = woothee.parse(ua_string)
-        csvwriter.writerow([
+        writer.writerow([
             ua_string,
-            "{0} {1}".format(woot['os'], woot['os_version']),
+            woot['os'],
+            woot['os_version'],
             device_type,
             woot['name'],
             woot['version']])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(sys.argv[1:])
